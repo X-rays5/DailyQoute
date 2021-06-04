a068800546716e2e765936269c528fe729b7b7bf
X-rays5 <61073708+X-rays5@users.noreply.github.com>
2021-06-04 11:43:36 +0200

    removed some useless code

--- DailyQoute/main.cpp
+++ DailyQoute/main.cpp
@@ -12,43 +12,6 @@ const char* wchartochar(WCHAR* w) {
 	return (const char*)w;
 }
 
-DWORD GetProcId(std::string procname)
-{
-	DWORD procId = 0;
-	HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-
-	if (hSnap != INVALID_HANDLE_VALUE)
-	{
-		PROCESSENTRY32 procEntry;
-		procEntry.dwSize = sizeof(procEntry);
-
-		if (Process32First(hSnap, &procEntry))
-		{
-			do
-			{
-				if (!_stricmp(wchartochar(procEntry.szExeFile), procname.c_str()))
-				{
-					procId = procEntry.th32ProcessID;
-					break;
-				}
-			} while (Process32Next(hSnap, &procEntry));
-		}
-	}
-	CloseHandle(hSnap);
-	return procId;
-}
-
-bool IsForegroundProcess(DWORD pid)
-{
-	HWND hwnd = GetForegroundWindow();
-	if (hwnd == NULL) return false;
-
-	DWORD foregroundPid;
-	if (GetWindowThreadProcessId(hwnd, &foregroundPid) == 0) return false;
-
-	return (foregroundPid == pid);
-}
-
 void MainProgram() {
 	cpr::Response r = cpr::Get(cpr::Url{ "http://api.quotable.io/random" });
 	rapidjson::Document json;
