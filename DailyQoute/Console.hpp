df87dc2563d2871d9609edbe60af8e817264adfb
Restyled.io <commits@restyled.io>
2021-10-06 20:28:33 +0000

    Restyled by clang-format

--- DailyQoute/Console.hpp
+++ DailyQoute/Console.hpp
@@ -1,29 +1,29 @@
 #pragma once
+#include <Windows.h>
+#include <filesystem>
 #include <fstream>
 #include <iostream>
-#include <Windows.h>
-#include <vector>
 #include <mutex>
-#include <filesystem>
 #include <string>
+#include <vector>
 
 namespace DailyQoute {
 using std::string;
 class console {
 public:
-    void Init();
+  void Init();
 
-    // Send message
-    void Send(string text);
-    void Send(string text, int color);
-    void SendDebug(string text);
-    void SendDebug(string text, int color);
+  // Send message
+  void Send(string text);
+  void Send(string text, int color);
+  void SendDebug(string text);
+  void SendDebug(string text, int color);
 
-    // Input
-    std::string In();
-    std::string In(string text);
-    std::string In(string text, int color);
+  // Input
+  std::string In();
+  std::string In(string text);
+  std::string In(string text, int color);
 };
 
 inline std::unique_ptr<console> Console;
-}
+} // namespace DailyQoute
